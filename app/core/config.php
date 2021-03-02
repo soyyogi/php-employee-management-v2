b33bce26f8ca958c0122b3def99196603a04599c
soyyogi <raoyogesh78@yahoo.in>
2021-03-02 05:50:09 +0100

    Create config file

---
+++ app/core/config.php
@@ -0,0 +1,16 @@
+<?php
+
+//Base Path
+define("BASE_PATH", dirname(__FILE__, 3));
+
+//CONTROLLERS
+define("CONTROLLERS", BASE_PATH . '/app/controllers/');
+
+//CORE
+define("CORE", BASE_PATH . '/app/core/');
+
+//VIEWS
+define("VIEWS", BASE_PATH . '/app/views/');
+
+//MODELS
+define("MODELS", BASE_PATH . '/app/models/');
