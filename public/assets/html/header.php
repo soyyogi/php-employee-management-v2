be4a1c62d9399a69b16773570a21f029742e4f87
soyyogi <raoyogesh78@yahoo.in>
2021-03-04 11:30:36 +0100

    update header

--- public/assets/html/header.php
+++ public/assets/html/header.php
@@ -1,5 +1,14 @@
-<div id="header" class="header">
-    <h2 class="header__title">Employee Manager</h2>
-    <div id="header-path" class="header-path"></div>
-    <div id="header-logout" class="header-logout"></div>
-</div>
+<header class="header_container">
+    <div class="header_container_div">
+        <button class="header_container_div__btn" id='index'></button>
+        <h1 class="header_container_div__title">Employees Management</h1>
+        <div class="header_container_div__currentPage">
+            <a href="http://localhost/php-employee-management-v2/employee" class="header_container_div_currentPage__dashboard">Dashboard</a>
+            <a href="http://localhost/php-employee-management-v2/employee/add"
+                class="header_container_div_currentPage__employee">Employee</a>
+        </div>
+    </div>
+    <div class="header_container_logout">
+        <a href="http://localhost/php-employee-management-v2/login/die" class="header_container_logout__btn">Logout</a href="">
+    </div>
+</header>
